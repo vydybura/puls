79e179204541155f20a53323cb24d17a1de06ec9
vydybura <105645207+vydybura@users.noreply.github.com>
2022-07-02 23:41:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
       speed: 1200,
       adaptiveHeight: true,
@@ -15,13 +15,14 @@ $(document).ready(function(){
         }
       ]
     });
-   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-   });
-  
-      function toggleSlide(item) {
+    });
+    
+    function toggleSlide(item) {
           $(item).each(function(i) {
             $(this).on('click', function (e) {
               e.preventDefault();
@@ -34,23 +35,21 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
-    //Modal
-  
-    $('[data-modal=consultation]').on('click', function () {
+      $('[data-modal=consultation]').on('click', function () {
       $('.overlay,#consultation').fadeIn('slow');
     });
     $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
- 
+
     $('.button_mini').each(function (i) {
       $(this).on('click', function () {
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('slow');
       })
-      });
-  
-      function valideForms(form) {
+    });
+    
+    function valideForms(form) {
         $(form).validate({
         rules: {
           name: "required",
@@ -59,11 +58,8 @@ $(document).ready(function(){
             required: true,
             email:true
           }
-          },
-          
-
-
-        messages: {
+          },    
+         messages: {
           name: {
           required: "Пожалуйста введите своё имя",
           minlength: jQuery.validator.format("Введите {0} символа!")
@@ -76,29 +72,28 @@ $(document).ready(function(){
       }
       });
     };
-  
     valideForms('#consultation-form');
     valideForms('#consultation form');
     valideForms('#order form');
-
     $('input[name=phone]').mask("+38 (099) 99-99-999 ");
-  
-    $('form').sumit(function (e) {
-      e.preventDefault();
-      $.ajax({
-        type: "POST",
-        url: "mailer/smart.php",
-        data: $(this).serialize()
-      }).done(function () {
-        $(this).find("input").val("");
 
 
-        $('form').trigger('reset');
-      });
-      return false;
+    
+
+ $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
     });
- 
-   
-  
-  
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+
 });  
